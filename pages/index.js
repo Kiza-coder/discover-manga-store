cd5ee2fba6463ac8db81acdfe097787bc9078f30
Kiza-coder <kiza.motoko@gmail.com>
2022-04-12 13:00:31 +0200

    Update Save

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Banner from '../components/Banner.js';
 import Image from 'next/image';
 import Card from '../components/Card.js';
 
+import mangasStores from '../data/manga-store.json';
+
+
 export default function Home() {
 
   const handleOnButtonClick = (event) => {
@@ -31,12 +34,19 @@ export default function Home() {
             height={400}
           />
         </div>
-
-        <Card 
-          name="Dark Manga Store"
-          href="/" 
-          imgUrl="/static/images/hero-image.png"
-        />
+        <div className={styles.cardLayout}>
+          { mangasStores.map(mangaStore => {
+            return (
+              <Card 
+              name= {mangaStore.name}
+              href="/" 
+              imgUrl="/static/images/hero-image.png"
+              className= {styles.card}
+              />
+              );
+            })
+          }
+        </div>
         
       </main>
     </div>
