0061b16a5d6d0ad4e0ee53c59febc22638b55542
Kiza-coder <kiza.motoko@gmail.com>
2022-05-12 16:34:58 +0200

    Update

--- pages/manga-store/[id].js
+++ pages/manga-store/[id].js
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-
 import mangasStoresData from '../../data/manga-store.json';
+import Head from 'next/head';
+import styles from '../../styles/manga-store.module.css'
+import Image from 'next/image';
 
 
 export function getStaticProps(staticProps){
     const params =  staticProps.params;
-    console.log(params)
+    
     return {
         props: {
             mangaStore: mangasStoresData.find(mangaStore => {
@@ -16,32 +18,46 @@ export function getStaticProps(staticProps){
 }
 
 export function getStaticPaths() {
+    const paths = mangasStoresData.map(mangaStore => {
+        return {
+            params:{
+                id: mangaStore.id.toString()
+            }
+        }
+    })
+
     return {
-        paths: [
-            { params: {id: "1"}},
-            { params: {id: "2"}},
-        ],
-        fallback: false
+        paths,
+        fallback: true
     }
 }
 
-const MangaStore = (props) => {
-    const router = useRouter(); 
-    console.log(props)
+const MangaStore = ({ mangaStore}) => {
+    const router = useRouter();
+    const {id, name, imgUrl, address} = mangaStore; 
 
-    return(<div>
+    if (router.isFallback) {
+        return <div>Loading ......</div>;
+    }
+
+    return(
+    <div className={styles.layout}>
+        <Head>
+            <title>{name}</title>
+        </Head>
         Manga Store :  {router.query.id}
-        <Link href="/">
-            <a>Back to home</a>
-        </Link> 
-        <p>{props.mangaStore.id}</p>
-        <p>{props.mangaStore.name}</p>       
-        <p>{props.mangaStore.imgUrl}</p>              
-        <p>{props.mangaStore.address}</p>       
-       
-    </div>
-
-    );
+        <div className={styles.col1}>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link> 
+            <p>{name}</p>
+            <Image src={imgUrl} width={600} height={360} className={styles.storeImg} alt={name}/>
+        </div>
+        <div className={styles.col2}>     
+            <p>{imgUrl}</p>              
+            <p>{address}</p>   
+        </div>      
+    </div>);   
 };
 
 export default MangaStore;
