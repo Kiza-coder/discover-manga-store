b61f723d9c7647d9bc06b8ae50730e81f595a3fb
Kiza-coder <kiza.motoko@gmail.com>
2022-05-25 09:49:04 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,15 @@ import mangasStoresData from '../data/manga-store.json';
 
 
 export async function getStaticProps(context) {
-
-    fetch("https://foursquare.com/oauth2/authenticate?client_id=DLWX22NAM2MHQKJRIE4OML55HJZVAL0VMNTMQZGDCI4LKHSZ&response_type=code&redirect_uri=http://localhost:3000")
-    .then(response => console.log(response))
+   
+  //fetch("https://foursquare.com/oauth2/authenticate?client_secret=2QWFL4XYRKVUTIR3P5YWIQLP0C342KIAWQP2AUPOVHXW3RWR&client_id=DLWX22NAM2MHQKJRIE4OML55HJZVAL0VMNTMQZGDCI4LKHSZ&response_type=code&redirect_uri=http://localhost:3000")
+  //.then(response => console.log(response))
+          console.log('STARTT')
+         fetch('https://foursquare.com/oauth2/access_token?client_id=DLWX22NAM2MHQKJRIE4OML55HJZVAL0VMNTMQZGDCI4LKHSZ&client_secret=2QWFL4XYRKVUTIR3P5YWIQLP0C342KIAWQP2AUPOVHXW3RWR&grant_type=code&redirect_uri=http://localhost:3000&code=EVRAT2L2QZYQDX4J4Z5GFH21DJVXXMNTIJIVGZDJLYHS3SK4#_=_',
+          {method: 'POST',
+          headers: {Accept: 'application/json'},
+          mode: 'no-cors', })
+          .then(response => console.dir(response  ))
    
     
 
